dcff8958dbf376ed658917242bcce8ca22d73269
yenkuanlee <yenkuanlee@gmail.com>
2018-03-15 09:26:07 +0000

    app solidity update

--- Application/app.sol
+++ Application/app.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.4.0;
 
 contract App {
 
+    mapping(bytes24 => string) public app;
+
     function App() public{
+        app["Vote"] = "QmWvMxzFoKjUh4nF9Knf5XSYguVgzArzVZcsNXUJComLvD";
     }
 
-    mapping(bytes24 => string) public app;
-    
     function setNode(bytes24 appName, string Ohash){
 	app[appName] = Ohash;
     }
