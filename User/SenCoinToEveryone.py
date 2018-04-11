ba5867c3f45743623941f6b5e0c4a6755d9d9e22
Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-04-11 07:29:57 +0000

    update god

--- User/SenCoinToEveryone.py
+++ User/SenCoinToEveryone.py
@@ -8,4 +8,5 @@ while True:
     user = line.split("\t")[1].split("\n")[0]
     web3.personal.unlockAccount(god, '123')
     web3.eth.sendTransaction({"to": user, "from": god,"value":web3.toWei("1000", "ether")})
+    time.sleep(1)
 f.close()
