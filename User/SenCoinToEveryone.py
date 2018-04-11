f1baa6fab294fccfa73f1ab467ef350a0c159f39
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-04-11 14:47:41 +0800

    update user

--- User/SenCoinToEveryone.py
+++ User/SenCoinToEveryone.py
@@ -1,10 +1,11 @@
 from web3 import Web3, HTTPProvider
-web3 = Web3(HTTPProvider('http://150.117.122.81:8545'))
-god = "0x3b48ba756fb58492ea9cfca48df123fc09bee72b"
+web3 = Web3(HTTPProvider('http://150.117.122.84:8545'))
+god = "0xe55879e12514f2255e6f4fd36bd459f0807de3b2"
 f = open("AllUser.txt","r")
 while True:
     line = f.readline()
+    if not line:break
     user = line.split("\t")[1].split("\n")[0]
     web3.personal.unlockAccount(god, '123')
-    web3.eth.sendTransaction({"to": user, "from": god,"value":web3.toWei("10000", "ether")})
+    web3.eth.sendTransaction({"to": user, "from": god,"value":web3.toWei("1000", "ether")})
 f.close()
