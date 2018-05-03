9e92225f4639acb03735fa15b08490015e65ebfd
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-05-03 13:42:33 +0800

    deploy application

--- depoly.py
+++ depoly.py
@@ -1,5 +1,5 @@
 import os
+os.system("sudo /opt/tomcat7/bin/startup.sh")
 os.system("ipfs daemon &")
 os.system("echo '{ \"Data\": \"Vote\" }' | ipfs object put")
 os.system("ipfs pin add QmWvMxzFoKjUh4nF9Knf5XSYguVgzArzVZcsNXUJComLvD")
-os.system("sudo /opt/tomcat7/bin/startup.sh")
